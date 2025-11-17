96573a8d472b0f0bf06715c4e087b789b0c3b294
raaakib0 <raaakib0@gmail.com>
2025-11-17 21:14:13 +0600

    api function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        return User::create([
+            'name' => $request -> name,
+            'email' => $request -> email,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->get('/test',function(){
     return response()->json(['message'=>'API is working']);
 });
+
+Route::get('/users',[UserController::class, 'index']);
+Route::post('/users',[UserController::class, 'store']);
