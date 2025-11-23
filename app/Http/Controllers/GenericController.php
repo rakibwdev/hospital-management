3e34661cfe998625791f091231ecb339e041f1d7
raaakib0 <raaakib0@gmail.com>
2025-11-23 21:57:07 +0600

    add generic controller

--- app/Http/Controllers/GenericController.php
+++ app/Http/Controllers/GenericController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Generic;
 use Illuminate\Http\Request;
 
 class GenericController extends Controller
 {
-    //
+    public function index()
+    {
+        $generics = Generic::all();
+        return view('generics.index', compact('generics'));
+    }
 }
