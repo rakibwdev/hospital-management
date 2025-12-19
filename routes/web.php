43418aa1c446d237b1dab344fc45b2af3e43947d
raaakib0 <raaakib0@gmail.com>
2025-12-19 20:22:14 +0600

    connect dashboard path

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,11 @@
 
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GenericController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('Dashboard');
-});
+Route::get('/', [DashboardController::class, 'index']);
 Route::get('/generics', [GenericController::class,'index']);
 Route::get('/brands', [BrandController::class,'brands'] );
 Route::get('/companies', [CompanyController::class,'company'] );
