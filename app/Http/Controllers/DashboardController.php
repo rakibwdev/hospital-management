3e098a71654655abb658e9dd38b26bcc352c948b
raaakib0 <raaakib0@gmail.com>
2026-01-01 19:38:45 +0600

    dashboard controller formated

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $brandCount = \App\Models\Brand::count();
-        $companyCount =\App\Models\Company::count();
-        $genericCount=\App\Models\Generic::count();
+        $companyCount = \App\Models\Company::count();
+        $genericCount = \App\Models\Generic::count();
 
         return view('dashboard', compact('brandCount', 'companyCount', 'genericCount'));
     }
