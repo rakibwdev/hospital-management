c53dc75235bd5b68a2c6acb895adb15445a216c1
raaakib0 <raaakib0@gmail.com>
2026-01-13 21:59:40 +0600

    cleaned

--- app/Http/Controllers/GenericController.php
+++ app/Http/Controllers/GenericController.php
@@ -5,15 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Generic;
 use Illuminate\Http\Request;
 
-// class GenericController extends Controller
-// {
-//     public function index()
-//     {
-//         $generics = Generic::all();
-//         return view('generics.genericView', compact('generics'));
-//     }
-// }
-
 class GenericController extends Controller
 {
     public function index()
