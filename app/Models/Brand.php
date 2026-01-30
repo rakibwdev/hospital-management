52f1ccbb26d27411a195bc5875ceb09d10bf944f
raaakib0 <raaakib0@gmail.com>
2026-01-30 21:26:12 +0600

    query generic id

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -38,6 +38,7 @@ class Brand extends Model
         })->when($company_id, function ($query) use ($company_id) {
             $query->where('company_id', $company_id);
         })->when($generic_id, function ($query) use ($generic_id) {
+            // $query->where('generic_id', $generic_id);
             $query->where('generic_id', $generic_id);
         });
     }
